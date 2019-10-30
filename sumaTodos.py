69ff7876405fa8c2b852f64c2d4ed405e72caa72
Vfuentealba <57098318+Vfuentealba@users.noreply.github.com>
2019-10-30 14:11:32 -0300

    Create sumaTodos.py

---
+++ sumaTodos.py
@@ -0,0 +1,18 @@
+def sumaTodos(limitTo):
+    resultado = 0
+    for i in range(0, limitTo+1):
+        resultado += i
+        
+    return resultado
+
+    
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    for i in range(0, limitTo+1):
+        resultado += i*i
+        
+    return resultado
+    
+print(sumaTodos(100))
+print(sumaTodosLosCuadrados(100))
+            
